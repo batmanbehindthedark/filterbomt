3e1cb4868264fb070ef2115cabeeba95dc4d691e
batmanbehindthedark <129989819+batmanbehindthedark@users.noreply.github.com>
2023-04-15 14:43:23 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("🔰Join Channel🔰", url="https://t.me/malluinstafollowers"),
+                    InlineKeyboardButton("🔰Join Channel🔰", url="https://t.me/KumarValimaiOfcl"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("🔰Join Group🔰", url="https://t.me/malluinstaufollowers"),
-                    InlineKeyboardButton("details about Group", url="https://t.me/Malluinstabot")
+                    InlineKeyboardButton("🔰Join Group🔰", url="https://t.me/KumarValimaiOfcl"),
+                    InlineKeyboardButton("details about Group", url="https://t.me/KumarValimaibot")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/DarkDevil1265/Unlimited-filter-bot-RJ")
+                        "SOURCE CODE", url="https://t.me/KumarValimaiOfcl")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
